df44851e6a453f9f7cfb9e0f4d12fd61706e4aa0
Tomas Scerbak <tomas.scerbak@gmail.com>
2023-03-01 17:54:49 +0100

    created searchbar componentn

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,21 @@
+import classes from "./SearchBar.module.css";
+
+import SearchIcon from "../assets/icon-search.svg";
+
+const SearchBar = () => {
+  return (
+    <div className={classes["search-wrapper"]}>
+      <label htmlFor="search">
+        <img src={SearchIcon} />
+      </label>
+      <input
+        id="search"
+        className={classes.search}
+        type="text"
+        placeholder="Search for movies or TV series"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/components/pages/RootLayout.jsx
+++ src/components/pages/RootLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../Navigation";
+import SearchBar from "../SearchBar";
 
 const RootLayout = () => {
   return (
     <div>
       <Navigation />
       <main>
+        <SearchBar />
         <Outlet />
       </main>
     </div>
