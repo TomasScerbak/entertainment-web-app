167fc2052910443b05c14b2d44760e3838cb4cb3
Tomas Scerbak <tomas.scerbak@gmail.com>
2023-03-04 15:17:42 +0100

    adding overlay class to trading card component

--- src/components/UI/TrendingCard.jsx
+++ src/components/UI/TrendingCard.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import BookmarkIconEmpty from "../../assets/icon-bookmark-empty.svg";
 import CategoryMovieImg from "../../assets/icon-category-movie.svg";
+import PlayIcon from "../../assets/icon-play.svg";
 
 import classes from "./TrendingCard.module.css";
 
 const TrendingCard = ({ title, image }) => {
+  const [hovered, setHovered] = useState(false);
+
+  const toggleHoverHandler = () => {
+    setHovered(!hovered);
+  };
   return (
-    <div className={classes["trending-card"]}>
-      <img src={image} className={classes["trending-image"]} />
+    <div
+      onMouseEnter={toggleHoverHandler}
+      onMouseLeave={toggleHoverHandler}
+      className={classes["trending-card"]}
+    >
+      {hovered && (
+        <div className={hovered ? `${classes.overlay}` : ""}>
+          <img src={PlayIcon} alt="#" />
+        </div>
+      )}
+      <img
+        src={image}
+        className={classes["trending-image"]}
+        alt="movie poster"
+      />
       <div className={classes["bookmark-icon-wrapper"]}>
         <img src={BookmarkIconEmpty} alt="bookmark icon" />
       </div>
