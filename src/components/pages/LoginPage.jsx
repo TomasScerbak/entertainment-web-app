1441d07cf254c33d7ec9161e19fb257a8d95264e
Tomas Scerbak <tomas.scerbak@gmail.com>
2023-03-07 13:48:35 +0100

    added login page

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import Logo from "../../assets/logo.svg";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <section>
+      <img src={Logo} alt="logo" />
+      <form>
+        <h1>Login</h1>
+        <input type="email" />
+        <input type="password" name="password" id="password" />
+      </form>
+    </section>
+  );
 };
 
 export default LoginPage;
--- src/components/pages/SignUpPage.jsx
+++ src/components/pages/SignUpPage.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <section>
+      <div>
+        <header>
+          <h1>Sign Up</h1>
+        </header>
+        <form>
+          <input type="email" name="email" />
+          <input type="password" name="password" />
+          <input type="password" name="repeat-password" id="" />
+        </form>
+        <button>Create an account</button>
+        <footer>
+          <p>Already have an account?</p>
+          <Link to="/login">Login</Link>
+        </footer>
+      </div>
+    </section>
+  );
 };
 
 export default SignUpPage;
