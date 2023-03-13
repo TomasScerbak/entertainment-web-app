e33ffec65b4b31faa15116bbc5238c3677d32234
Tomas Scerbak <tomas.scerbak@gmail.com>
2023-03-13 09:00:58 +0100

    pulling out pathname from location object

--- src/components/pages/RootLayout.jsx
+++ src/components/pages/RootLayout.jsx
@@ -2,14 +2,11 @@ import { Outlet, useLocation } from "react-router-dom";
 import Navigation from "../Navigation";
 
 const RootLayout = () => {
-  const location = useLocation();
+  const { pathname } = useLocation();
 
   return (
     <>
-      {location.pathname === "/login" ||
-      location.pathname === "/signup" ? null : (
-        <Navigation />
-      )}
+      {pathname === "/login" || pathname === "/signup" ? null : <Navigation />}
       <main>
         <Outlet />
       </main>
