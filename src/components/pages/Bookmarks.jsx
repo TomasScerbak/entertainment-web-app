9deb43409661140645636ef80afa8f46efad5836
Tomas Scerbak <tomas.scerbak@gmail.com>
2023-03-28 17:03:35 +0200

    displaying logic for bookmarked movies

--- src/components/pages/Bookmarks.jsx
+++ src/components/pages/Bookmarks.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+import SearchBar from "../UI/SearchBar";
 import MovieCard from "../UI/MovieCard";
 import Modal from "../UI/ModalCard";
+import MovieSection from "../MovieSection";
 
 const Bookmarks = () => {
   const user = useSelector((state) => state.auth.value);
@@ -14,26 +16,49 @@ const Bookmarks = () => {
   };
 
   return (
-    <section>
-      {user ? (
-        <>
-          {bookmarkedMovie.map((movie) => (
-            <MovieCard
-              isBooked={!!findBookmarkedMovie(movie.id)}
-              key={movie.id}
-              id={movie.id}
-              title={movie.title || movie.name}
-              image={movie.image}
-              category={movie.type}
-              year={movie.year}
-              rating={movie.rating}
-            />
-          ))}
-        </>
-      ) : (
-        <Modal />
-      )}
-    </section>
+    <>
+      <SearchBar placeholder="Search for bookmarked shows" />
+      <>
+        {bookmarkedMovie.length === 0 ? (
+          <MovieSection title={"You have no bookmarked movies"} />
+        ) : (
+          <>
+            <MovieSection title={"Bookmarked Movies"}>
+              {bookmarkedMovie.map((movie) =>
+                movie.category === "movie" ? (
+                  <MovieCard
+                    isBooked={!!findBookmarkedMovie(movie.id)}
+                    key={movie.id}
+                    id={movie.id}
+                    title={movie.title || movie.name}
+                    image={movie.image}
+                    category={movie.type}
+                    year={movie.year}
+                    rating={movie.rating}
+                  />
+                ) : null
+              )}
+            </MovieSection>
+            <MovieSection title={"Bookmarked TV series"}>
+              {bookmarkedMovie.map((movie) =>
+                movie.category === "tv" ? (
+                  <MovieCard
+                    isBooked={!!findBookmarkedMovie(movie.id)}
+                    key={movie.id}
+                    id={movie.id}
+                    title={movie.title || movie.name}
+                    image={movie.image}
+                    category={movie.type}
+                    year={movie.year}
+                    rating={movie.rating}
+                  />
+                ) : null
+              )}
+            </MovieSection>
+          </>
+        )}
+      </>
+    </>
   );
 };
 
